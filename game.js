3d3889e900bdd1313734c2c5f02a8aaf0b32357e
dzagidulin <dmitri@zagidulin.net>
2012-04-21 12:58:54 -0400

    Implement projectiles with range

--- game.js
+++ game.js
@@ -1,99 +1,18 @@
 var game = {
 	creatureHeight: 17,
 	playerSpeed: 2,
+	projectileSpeed: 2,
 	groundX: 200,
 	groundY: 330,
 	groundWidth: 450,
-	groundHeight: 10
+	groundHeight: 10,
+	projectileCooldown: 300,
+	gameAreaMinX: 10,
+	gameAreaMaxX: 890,
+	gameAreaMinY: 10,
+	gameAreaMaxY: 490
 }
-function Creature(options) {
-	this.x = options.x
-	this.y = options.y
-	this.height = game.creatureHeight
-	this.my_rect = new jaws.Rect(this.x, this.y, this.height, this.height);
-	this.speed = 1
-}
-Creature.prototype.move = function(dxdy) {
-	dx = dxdy.x * this.speed
-	dy = dxdy.y * this.speed
-	this.rect().move(dx, dy);
-}
-Creature.prototype.rect = function() {
-	return this.my_rect;
-}
-
-function Player(options) {
-	options.x = game.groundX + 10
-	options.y = game.groundY
-
-	Creature.call(this, options) // Use parent's constructor
-
-	this.speed = options.speed
-	
-	
-	this.draw = function() {
-		drawText(20, 'White', 'A', this.rect().x, this.rect().y)
-	}
-}
-Object.extend(Player, Creature)
-
-function Enemy(options) {
-
-	Creature.call(this, options) // Use parent's constructor
-
-	this.draw = function() {
-		drawText(17, 'Red', 'A', this.rect().x, this.rect().y)
-		// draw collision rect
-		square_side = this.height
-		jaws.context.lineWidth = 0.5
-		jaws.context.strokeStyle = "gray"
-		jaws.context.strokeRect(this.rect().x, this.rect().y - this.height,
-				square_side, square_side)
-	}
-}
-Object.extend(Enemy, Creature)
-
-/**
- * GameState is the actual game play. We switch to it once user choses "Start game"
- *
- */
-
-function GameState() {
-	this.setup = function() {
-		jaws.on_keydown("esc", function() {
-			jaws.switchGameState(MenuState)
-		})
-		
-		game.player = new Player({
-			x : 20,
-			y : 20,
-			speed: game.playerSpeed
-		})
-	}
-
-	this.draw = function() {
-		// Clear screen
-		jaws.context.fillStyle = "black"
-		jaws.context.fillRect(0, 0, jaws.width, jaws.height)
-
-		this.drawGround()
-		game.player.draw()
-	}
-
-	this.drawGround = function() {
-		var x = game.groundX
-		var y = game.groundY
-		var height = game.groundHeight
-		var width = game.groundWidth
-
-		jaws.context.strokeStyle = "white"
-		jaws.context.fillStyle = "blue"
-		jaws.context.lineWidth = 1
-		jaws.context.fillRect(x, y, width, height)
-		jaws.context.strokeRect(x, y, width, height)
-	}
-
-	this.direction_keys = {
+var direction_keys = {
 		left : {
 			x : -1,
 			y : 0
@@ -110,19 +29,157 @@ function GameState() {
 			x : 0,
 			y : 1
 		}
+}
+function isOutsideCanvas(item) {
+	return (item.x < game.gameAreaMinX || item.y < game.gameAreaMinY
+			|| item.x > game.gameAreaMaxX || item.y > game.gameAreaMaxY)
+}
+
+function Thing(options) {
+	this.x = options.x
+	this.y = options.y
+	this.height = game.creatureHeight
+	this.my_rect = new jaws.Rect(this.x, this.y, this.height, this.height);
+	this.speed = 1
+}
+Thing.prototype.move = function(dxdy) {
+	dx = dxdy.x * this.speed
+	dy = dxdy.y * this.speed
+	this.rect().move(dx, dy);
+}
+Thing.prototype.rect = function() {
+	return this.my_rect;
+}
+
+function Projectile(options) {
+	Thing.call(this, options) // Use parent's constructor
+	this.startX = options.x
+	this.startY = options.y
+	this.speed = game.projectileSpeed
+	
+	this.range = options.range
+	
+	this.draw = function() {
+		drawText(20, 'White', '*', this.rect().x, this.rect().y)
+	}
+	
+	this.update = function() {
+		dxdy = direction_keys['right']
+		this.move(dxdy)
+	}
+}
+Object.extend(Projectile, Thing)
+
+Projectile.prototype.isInRange = function() {
+	return this.rect().x < (this.startX + this.range)
+}
+
+Projectile.isOutsideRange = function(item) {
+	return !item.isInRange()
+}
+
+
+
+function Player(options) {
+	options.x = game.groundX + 10
+	options.y = game.groundY
+
+	Thing.call(this, options) // Use parent's constructor
+
+	this.speed = options.speed
+	
+	
+	this.draw = function() {
+		drawText(20, 'White', 'A', this.rect().x, this.rect().y)
+	}
+}
+Object.extend(Player, Thing)
+
+function Enemy(options) {
+
+	Thing.call(this, options) // Use parent's constructor
+
+	this.draw = function() {
+		drawText(17, 'Red', 'A', this.rect().x, this.rect().y)
+		// draw collision rect
+		square_side = this.height
+		jaws.context.lineWidth = 0.5
+		jaws.context.strokeStyle = "gray"
+		jaws.context.strokeRect(this.rect().x, this.rect().y - this.height,
+				square_side, square_side)
+	}
+}
+Object.extend(Enemy, Thing)
+
+/**
+ * GameState is the actual game play. We switch to it once user choses "Start game"
+ *
+ */
+
+function GameState() {
+	var playerProjectiles = new jaws.SpriteList()
+	
+	this.setup = function() {
+		jaws.on_keydown("esc", function() {
+			jaws.switchGameState(MenuState)
+		})
+		jaws.preventDefaultKeys(["left", "right", "space"])
+		
+		game.player = new Player({
+			x : 20,
+			y : 20,
+			speed: game.playerSpeed
+		})
+		game.player.can_fire = true
+	}
+
+	this.draw = function() {
+		// Clear screen
+		jaws.context.fillStyle = "black"
+		jaws.context.fillRect(0, 0, jaws.width, jaws.height)
+
+		this.drawGround()
+		game.player.draw()
+		playerProjectiles.draw()
+	}
+
+	this.drawGround = function() {
+		var x = game.groundX
+		var y = game.groundY
+		var height = game.groundHeight
+		var width = game.groundWidth
+
+		jaws.context.strokeStyle = "white"
+		jaws.context.fillStyle = "blue"
+		jaws.context.lineWidth = 1
+		jaws.context.fillRect(x, y, width, height)
+		jaws.context.strokeRect(x, y, width, height)
 	}
 	
 	this.update = function() {
 		var player = game.player
 		var dxdy
 		if (jaws.pressed("left")) {
-			dxdy = this.direction_keys['left']
+			dxdy = direction_keys['left']
 			player.move(dxdy)
 		}
 		if (jaws.pressed("right")) {
-			dxdy = this.direction_keys['right']
+			dxdy = direction_keys['right']
 			player.move(dxdy)
 		}
+		if (jaws.pressed("space")) {
+			if(player.can_fire) {
+				bullet = new Projectile({x: player.rect().right, y:player.y, range:50})
+				bullet.collision = false
+
+				playerProjectiles.push(bullet)
+				player.can_fire = false
+				setTimeout(function() { game.player.can_fire = true }, game.projectileCooldown)
+			}
+		}
+		playerProjectiles.update()
+		
+		playerProjectiles.removeIf(Projectile.isOutsideRange)
 	}
 }
 
